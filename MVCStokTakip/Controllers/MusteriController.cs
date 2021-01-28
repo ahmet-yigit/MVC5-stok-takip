5b37ae1a7b9b02bcbb2b5154cf33180e0741dc27
Ahmet YİĞİT <ahmetyigit92@gmail.com>
2021-01-29 01:46:15 +0300

    .

---
+++ MVCStokTakip/Controllers/MusteriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStokTakip.Models.Entity;
+
+namespace MVCStokTakip.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        DbMvcStokTakipEntities db = new DbMvcStokTakipEntities();
+        public ActionResult Index()
+        {
+            var musteriListe = db.TBLMusteri.ToList();
+            return View(musteriListe);
+        }
+    }
+}
--- MVCStokTakip/Controllers/UrunController.cs
+++ MVCStokTakip/Controllers/UrunController.cs
@@ -13,8 +13,66 @@ namespace MVCStokTakip.Controllers
         DbMvcStokTakipEntities db = new DbMvcStokTakipEntities();
         public ActionResult Index()
         {
-            var dgr = db.TBLUrunler.ToList();
+            var dgr = db.TBLUrunler.Where(x => x.durum == true).ToList();
             return View(dgr);
+        }
+
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKategori.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.ad,
+                                                 Value = i.id.ToString()
+                                             }
+                                           ).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(TBLUrunler p)
+        {
+            var ktgr = db.TBLKategori.Where(x => x.id == p.TBLKategori.id).FirstOrDefault();
+            p.TBLKategori = ktgr;
+            db.TBLUrunler.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> urunKtgr = (from x in db.TBLKategori.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.ad,
+                                                 Value = x.id.ToString()
+                                             }).ToList();
+            var urun = db.TBLUrunler.Find(id);
+            ViewBag.urunKtgr = urunKtgr;
+            return View("UrunGetir", urun);
+        }
+        public ActionResult UrunGuncelle(TBLUrunler p)
+        {
+            var urun = db.TBLUrunler.Find(p.id);
+            urun.marka = p.marka;
+            urun.satisfiyat = p.satisfiyat;
+            urun.alisfiyat = p.alisfiyat;
+            urun.stok = p.stok;
+            urun.ad = p.ad;
+            var ktg = db.TBLKategori.Where(x => x.id == p.TBLKategori.id).FirstOrDefault();
+            urun.kategori = ktg.id;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunSil(TBLUrunler p)
+        {
+            var urun = db.TBLUrunler.Find(p.id);
+            urun.durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
