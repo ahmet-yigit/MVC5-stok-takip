5299f81dc543f1b0a9ab41529eda7d4bb01f46a2
Ahmet YİĞİT <ahmetyigit92@gmail.com>
2021-01-28 22:18:38 +0300

    proje gelişim aşamasında

---
+++ MVCStokTakip/Controllers/KategoriController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStokTakip.Models.Entity;
+namespace MVCStokTakip.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DbMvcStokTakipEntities db = new DbMvcStokTakipEntities();
+        public ActionResult Index()
+        {
+            var kategoriler = db.TBLKategori.ToList();
+            return View(kategoriler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKategori p)
+        {
+            db.TBLKategori.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = db.TBLKategori.Find(id);
+            db.TBLKategori.Remove(ktg);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKategori.Find(id);
+            return View("KategoriGetir", ktgr);
+        }
+
+
+        public ActionResult KategoriGüncelle(TBLKategori k)
+        {
+            var ktg = db.TBLKategori.Find(k.id);
+            ktg.ad = k.ad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCStokTakip/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStokTakip.Models.Entity;
+
+namespace MVCStokTakip.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        DbMvcStokTakipEntities db = new DbMvcStokTakipEntities();
+        public ActionResult Index()
+        {
+            var dgr = db.TBLUrunler.ToList();
+            return View(dgr);
+        }
+    }
+}
